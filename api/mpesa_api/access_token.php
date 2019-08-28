b445e2ac6d17909e626427f86f5b403ee2965fe8
stevekama <stevekamahertz@gmail.com>
2019-08-28 13:01:34 +0300

    change in mpesa access token

--- api/mpesa_api/access_token.php
+++ api/mpesa_api/access_token.php
@@ -11,8 +11,10 @@ $secret = $_POST['secret'];
 $auth = new Auth($key, $secret);
 
 $headers = ['Content-Type: application/json; charset=utf8'];
+
 $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 
 $access_token = $auth->Access_Token($headers, $url);
+
 echo $access_token;
 ?>
