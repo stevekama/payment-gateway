db4ee905896aa3318bbd5af1089db195bed3bddd
harunmohamed <harunmohamed901@gmail.com>
2019-09-07 23:37:24 +0300

    Included APplications Manager page

---
+++ public/apps.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Applications Manager</title>
+    <link rel="stylesheet" href="jquery-ui-css">
+    <link rel="stylesheet" href="bootstrap.min.css"/>
+    <script src="jquery.min.js"></script>
+    <script src="jquery-ui.js"></script>
+</head>
+<?php require_once('../models/initialization.php'); 
+require_once('layouts/systems/header.php'); ?>
+<body>
+<section class="content-header">
+    <h1>
+      Allications
+      <small>Manage</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Manage apps</li>
+    </ol>
+  </section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once('layouts/systems/footer.php'); ?>
